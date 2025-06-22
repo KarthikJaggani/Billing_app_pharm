25a409e966f99dada2633ac5676220419102b3c8
KarthikJaggani <101697430+KarthikJaggani@users.noreply.github.com>
2025-06-22 13:15:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1430,5 +1430,9 @@ def sales_report():
         from_date=from_date,
         to_date=to_date
     )
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
